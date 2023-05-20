3cd8422f1b172536100a4a1fa1c54c637834b2fe
Dianova Arina <muffin_purper-gurk@mail.ru>
2023-05-20 16:23:50 +0300

    submit form

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-// document.querySelectorAll('.window__level-button').forEach(button => {
-//     button.addEventListener('click', () => {
-//         levelGameButton = this;
-//         console.log('1')
-//     });
-// });
+const form = document.forms.mainForm;
+const mainFormRadio = mainForm.difficult;
+//console.log(form, mainFormRadio)
 
-const levelGameButton = document.querySelectorAll('.window__level-button');
-const startGameButton = document.getElementById('start_btn')
-
-const initEventListener = () => {
-    for (const levelButton of levelGameButton) {
-        levelButton.addEventListener('click', () => {
-            const level = levelButton.dataset.level
-            console.log(level)
-        })
-    }
-}
-initEventListener();
-
-startGameButton.addEventListener('click', () => {
-    alert('выбранный уровень сложности'// + что то :D
-    )
-})
-
+form.addEventListener("submit", function (event) {
+    alert('выбранный уровень сложности ' + mainFormRadio.value);
+	if (!mainFormRadio.value) {
+		alert('Выберите уровень сложности!');
+	}
+    event.preventDefault();  
+});
 
