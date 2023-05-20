d26c37c312f33f6ccfc9e0fe09f7b6fdc5c3b707
Dianova Arina <muffin_purper-gurk@mail.ru>
2023-05-20 16:35:44 +0300

    забыла if

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const mainFormRadio = mainForm.difficult;
 //console.log(form, mainFormRadio)
 
 form.addEventListener("submit", function (event) {
-    alert('выбранный уровень сложности ' + mainFormRadio.value);
+	if(mainFormRadio.value){
+		alert('выбранный уровень сложности ' + mainFormRadio.value);
+	}
+    
 	if (!mainFormRadio.value) {
 		alert('Выберите уровень сложности!');
 	}
